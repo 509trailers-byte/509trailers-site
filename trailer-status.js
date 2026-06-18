de0de3a532e485227712d60554bb308ab4f92d60
509trailers-byte <509trailers@gmail.com>
2026-06-18 12:50:42 -0700

    Update trailer-status.js

--- trailer-status.js
+++ trailer-status.js
@@ -9,7 +9,7 @@
 var FLEET_STATUS = {
   'car-hauler':   { available: true,  bookedUntil: null },
   'dump-trailer': { available: true,  bookedUntil: null },
-  'deck-over':    { available: true,  bookedUntil: null },
+  'deck-over':    { available: false,  bookedUntil: 'June 20' },
   'enclosed':     { available: false, bookedUntil: 'June 20' }
 };
 
